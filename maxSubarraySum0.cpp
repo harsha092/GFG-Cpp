7158a3c31aed4d7e2ae5d2c83a9ae327815ece7b
Harsh <98465555+harsha092@users.noreply.github.com>
2023-09-16 23:29:45 +0530

    Create maxSubarraySum0.cpp

---
+++ maxSubarraySum0.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+/*You are required to complete this function*/
+
+class Solution{
+    public:
+    int maxLen(vector<int>&A, int n)
+    {   
+  unordered_map <int,int> m;
+int ans = 0,sum = 0;
+m[0] = -1;
+for(int i = 0;i < n;i++){
+    sum += A[i];
+    if(m.count(sum)) ans = max(ans,i-m[sum]);
+    else m[sum] = i;
+}
+return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int m;
+        cin>>m;
+        vector<int> array1(m);
+        for (int i = 0; i < m; ++i){
+            cin>>array1[i];
+        }
+        Solution ob;
+        cout<<ob.maxLen(array1,m)<<endl;
+    }
+    return 0; 
+}
+
+
+
+// } Driver Code Ends
