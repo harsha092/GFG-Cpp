e3ca618e48497e8c2d423d1eb6092652079853f0
Harsh <98465555+harsha092@users.noreply.github.com>
2023-10-23 17:22:28 +0530

    Create search in matrix.cpp

---
+++ search in matrix.cpp
@@ -0,0 +1,19 @@
+//User function template for C++
+class Solution{
+public:	
+	int matSearch (vector <vector <int>> &mat, int N, int M, int X)
+	{
+	 for(int i=0;i<N;i++)
+	    {
+	        for(int j=0;j<M;j++)
+	        {
+	            if(mat[i][j]==X)
+	            {
+	                return 1;
+	            }
+	        }
+	    }
+	    return 0;
+	           
+}
+};
