0f32a24428312487799f16fca94183c1fb6ad5cc
Harsh <98465555+harsha092@users.noreply.github.com>
2023-10-25 23:29:41 +0530

    Create pairs of certain diff.cpp

---
+++ pairs of certain diff.cpp
@@ -0,0 +1,15 @@
+class Solution{
+    public:
+    int maxSumPairWithDifferenceLessThanK(int arr[], int N, int K)
+    {
+        // Your code goes here  
+       
+        sort(arr,arr+N);
+         int s=0;
+        for(int i=N-1;i>0;i--)
+            if(arr[i]-arr[i-1]<K){
+                s=s+arr[i]+arr[i-1]; i--;
+            }
+         return s;
+    }
+};
