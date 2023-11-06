969b6d43ab690fd3548dcf15fdbaa55c1698c1f7
Harsh <98465555+harsha092@users.noreply.github.com>
2023-11-06 15:01:50 +0530

    Create Smallest sum contiguous subarray.cpp

---
+++ Smallest sum contiguous subarray.cpp
@@ -0,0 +1,14 @@
+class Solution{
+  public:
+  int smallestSumSubarray(vector<int>& a){
+      int sum=a[0];
+      int ans=a[0];
+      for(int i=1;i<a.size();i++)
+      {
+          sum=min(sum+a[i],a[i]);
+          ans=min(sum,ans);
+      }
+      return ans;
+    }
+ 
+};
