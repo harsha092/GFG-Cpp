56281b2206a175d3fda943c6bb46969b511a7f6d
Harsh <98465555+harsha092@users.noreply.github.com>
2023-12-21 02:52:39 +0530

    Create reverse coding.cpp

---
+++ reverse coding.cpp
@@ -0,0 +1,31 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int sumOfNaturals(int n) {
+        // code here
+        int mod=1e9+7;
+       long sum=(long)n*(long)(n+1);
+        sum/=2;
+        return sum%mod;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        
+        cin>>n;
+
+        Solution ob;
+        cout << ob.sumOfNaturals(n) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
