b3832ccda010b81dc2ff3dc6e838297d116de401
Harsh Raj <98465555+harsha092@users.noreply.github.com>
2024-06-02 20:59:42 +0530

    Create k largest.cpp

---
+++ k largest.cpp
@@ -0,0 +1,15 @@
+class Solution
+{
+    public:
+    //Function to return k largest elements from an array.
+    vector<int> kLargest(int arr[], int n, int k)
+    {
+        // code here
+        sort(arr,arr+n,greater<int>());
+        vector<int> ans;
+        for(int i=0;i<k;i++){
+            ans.push_back(arr[i]);
+        }
+        return ans;
+    }
+};
