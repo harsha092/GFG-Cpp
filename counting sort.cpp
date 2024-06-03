1784b7c75e0ee359e8ea1f605a350d83fb464b11
Harsh Raj <98465555+harsha092@users.noreply.github.com>
2024-06-04 01:36:27 +0530

    Create counting sort.cpp

---
+++ counting sort.cpp
@@ -0,0 +1,11 @@
+class Solution{
+    public:
+    //Function to arrange all letters of a string in lexicographical 
+    //order using Counting Sort.
+    string countSort(string arr){
+        // code here
+        sort(arr.begin(), arr.end());
+        return arr;
+    }
+    
+};
