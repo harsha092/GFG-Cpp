1acae3fef9045fde94e583776db3b27c24db3335
Harsh Raj <98465555+harsha092@users.noreply.github.com>
2024-06-05 00:58:52 +0530

    Update max index.cpp

--- max index.cpp
+++ max index.cpp
@@ -18,3 +18,31 @@ public:
     }
 
 };
+
+
+
+class Solution {
+public:
+    int maxIndexDiff(int arr[], int n) {
+        // Step 1: Precompute maximum values up to each index
+        std::vector<int> pref_max(n);
+        pref_max[n - 1] = arr[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            pref_max[i] = std::max(pref_max[i + 1], arr[i]);
+        }
+
+        // Step 2: Traverse from the start to find the maximum index difference
+        int ans = 0, j = 0;
+        for (int i = 0; i < n; i++) {
+            while (j < n && pref_max[j] < arr[i]) {
+                j++;
+            }
+            if (j < n) {
+                ans = std::max(ans, j - i);
+            }
+        }
+
+        return ans;
+    }
+};
+
