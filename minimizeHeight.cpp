30c3ed87f6397bc46b26b44fa0b564914ca1f8dd
Harsh Raj <98465555+harsha092@users.noreply.github.com>
2024-06-18 02:17:45 +0530

    Update minimizeHeight.cpp

--- minimizeHeight.cpp
+++ minimizeHeight.cpp
@@ -1,53 +1,16 @@
-//{ Driver Code Starts
-// Initial template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-// } Driver Code Ends
-// User function template for C++
-
 class Solution {
   public:
     int getMinDiff(int arr[], int n, int k) {
-     
         // code here
          sort(arr,arr+n);
-       int diff=arr[n-1]-arr[0];
-       
-       int least,most,mini,maxi;
-       least=arr[0]+k;
-       most=arr[n-1]-k;
-       
-       for(int i=0;i<n-1;i++){
-           mini=min(arr[i+1]-k,least);
-           maxi=max(arr[i]+k,most);
-           
-           if(mini<0)
-               continue;
-           else 
-               diff=min(diff,maxi-mini);
-       }
-       return diff;
+           int x=arr[n-1]-arr[0];
+           for(int i=1;i<n;i++){
+               if(arr[i]-k<0){ continue; }
+           int minn = min(arr[0]+k, arr[i]-k);
+           int maxx = max(arr[n-1]-k, arr[i-1]+k);
+           x = min(x,maxx-minn);
+           }
+    
+     return x;
     }
 };
-
-//{ Driver Code Starts.
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int n, k;
-        cin >> k;
-        cin >> n;
-        int arr[n];
-        for (int i = 0; i < n; i++) {
-            cin >> arr[i];
-        }
-        Solution ob;
-        auto ans = ob.getMinDiff(arr, n, k);
-        cout << ans << "\n";
-    }
-    return 0;
-}
-// } Driver Code Ends
