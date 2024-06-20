e7366676f8687f203cfdc701bc9f76c40ff69a54
Harsh Raj <98465555+harsha092@users.noreply.github.com>
2024-06-20 18:14:54 +0530

    Update smallest positive missing.cpp

--- smallest positive missing.cpp
+++ smallest positive missing.cpp
@@ -15,4 +15,22 @@ class Solution
     }
     
 };
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+class Solution
+{
+    public:
+    //Function to find the smallest positive number missing from the array.
+    int missingNumber(int arr[], int n) 
+    { 
+        // Your code here
+        sort(arr,arr+n);
+        int count=1;
+        for(int i=0;i<n;i++){
+        if(arr[i]<count) {continue;}
+        if(arr[i]==count) count++;
+        else  { return count;}
+        }
+        return count;
+    }
+};
 
